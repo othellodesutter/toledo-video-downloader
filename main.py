7b1c3fd78c08e55d95c600e7faadf5ca00396302
othellodesutter <othello.de.sutter@gmail.com>
2022-11-02 14:28:37 +0100

    added main

---
+++ main.py
@@ -0,0 +1,34 @@
+import json
+from fastapi import FastAPI, Request
+from fastapi.templating import Jinja2Templates
+from fastapi.staticfiles import StaticFiles
+from fastapi.encoders import jsonable_encoder
+import script
+
+app = FastAPI()
+templates = Jinja2Templates(directory="templates")
+app.mount("/static", StaticFiles(directory="static"), name="static")
+
+@app.get("/")
+def read_root(request: Request):
+    return templates.TemplateResponse('index.html', {'request': request})
+
+@app.post("/")
+async def main(request: Request):
+    data = await request.form()
+    data = jsonable_encoder(data)
+    requested_videos = data['videos']
+    try:
+        videos = script.get_video_metadata(script.get_kaltura_videos(requested_videos))
+        videos = json.loads(videos)
+
+        if videos == []:
+            return templates.TemplateResponse('index.html', {'request': request, 'error': "No videos found :'("})
+        else:
+            return templates.TemplateResponse('videos.html', {'request': request, 'videos': videos['videos']})
+    except:
+        return templates.TemplateResponse('index.html', {'request': request, 'error': "Error :("})
+
+@app.get("/info")
+def info(request: Request):
+    return templates.TemplateResponse('info.html', {'request': request})
